d66cf38d9b6130e97d87db9f43107d71a79e90bc
Steven Yuen <steven@mother.co>
2018-11-10 23:32:34 -0500

    initial commit

---
+++ src/View/index.js
@@ -0,0 +1,120 @@
+// =======================================================
+// View
+// Foundational component for structuring layouts
+// Rating: ?
+// Code to be simplified
+// =======================================================
+
+import React, { Component } from 'react'
+import { css, cx } from 'emotion'
+import PropTypes from 'prop-types'
+import generateStyleObject from '../utils/generateStyleObject'
+
+/* eslint-disable object-property-newline */
+@generateStyleObject(({
+   display, float, position, top, left, right, bottom, flex, flexFlow, justifyContent,
+   height, width, maxHeight, maxWidth, minHeight, minWidth,
+   margin, marginBottom, marginLeft, marginRight, marginTop,
+   padding, paddingHorizontal, paddingVertical,
+   paddingBottom, paddingLeft, paddingRight, paddingTop,
+   backgroundColor, scroll
+}) => {
+   const styleObject = {
+      display, float, position, top, left, right, bottom, flex, flexFlow, justifyContent,
+      height, width, maxHeight, maxWidth, minHeight, minWidth,
+      margin, marginBottom, marginLeft, marginRight, marginTop,
+      padding, paddingBottom, paddingLeft, paddingRight, paddingTop,
+      backgroundColor
+   }
+
+   if (scroll === 'on') {
+      styleObject.overflow = 'auto'
+      styleObject['-webkit-overflow-scrolling'] = 'touch'
+   }
+
+   if (paddingHorizontal) {
+      styleObject.paddingLeft = paddingHorizontal
+      styleObject.paddingRight = paddingHorizontal
+   }
+
+   if (paddingVertical) {
+      styleObject.paddingTop = paddingVertical
+      styleObject.paddingBottom = paddingVertical
+   }
+
+   return styleObject
+})
+/* eslint-enable */
+
+export default class View extends Component {
+   /* eslint-disable */
+   static propTypes = {
+      alignItems: PropTypes.string,
+      backgroundColor: PropTypes.string,
+      border: PropTypes.string,
+      borderTop: PropTypes.string,
+      borderRight: PropTypes.string,
+      borderBottom: PropTypes.string,
+      borderLeft: PropTypes.string,
+      children: PropTypes.node,
+      className: PropTypes.string,
+      display: PropTypes.string,
+      flex: PropTypes.string,
+      flexFlow: PropTypes.string,
+      float: PropTypes.string,
+      generatedStyleObject: PropTypes.object.isRequired,
+      height: PropTypes.string,
+      justifyContent: PropTypes.string,
+      margin: PropTypes.string,
+      marginTop: PropTypes.string,
+      marginLeft: PropTypes.string,
+      marginRight: PropTypes.string,
+      marginBottom: PropTypes.string,
+      minHeight: PropTypes.string,
+      maxHeight: PropTypes.string,
+      minWidth: PropTypes.string,
+      maxWidth: PropTypes.string,
+      onClick: PropTypes.func,
+      onScroll: PropTypes.func,
+      padding: PropTypes.string,
+      paddingTop: PropTypes.string,
+      paddingLeft: PropTypes.string,
+      paddingRight: PropTypes.string,
+      paddingBottom: PropTypes.string,
+      paddingHorizontal: PropTypes.string,
+      paddingVertical: PropTypes.string,
+      position: PropTypes.string,
+      scroll: PropTypes.string,
+      style: PropTypes.object,
+      top: PropTypes.string,
+      left: PropTypes.string,
+      right: PropTypes.string,
+      bottom: PropTypes.string,
+      width: PropTypes.string,
+   }
+   /* eslint-enable */
+
+   static defaultProps = {
+      display: 'block',
+      onScroll: () => {},
+      position: 'relative',
+      style: {}
+   }
+
+   render() {
+      const styles = {
+         ...this.props.style,
+         ...this.props.generatedStyleObject
+      }
+
+      return (
+         <div
+            ref={(node) => { this.node = node }}
+            className={cx(css(styles), this.props.className)}
+            onClick={this.props.onClick}
+            onScroll={this.props.onScroll}>
+            {this.props.children}
+         </div>
+      )
+   }
+}
