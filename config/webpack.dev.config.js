351857e60ac2bb629422673deacd7f795ca531e9
Ajay Sabhaney <ajay@mothercreative.com>
2018-11-18 14:39:02 -0500

    Some test setup, webpack setup, travisci setup, cleanup

--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -25,7 +25,6 @@ module.exports = {
             use: [
                'style-loader',
                'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
-               'resolve-url-loader',
                'less-loader'
             ]
          }
