4b1bf5c7b289636f5e6e46e3a01a6b6bf5e39554
Steven Yuen <steven@mother.co>
2018-11-19 16:30:55 -0500

    Added view component to docs

--- demo/index.js
+++ demo/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, NavLink, Route } from 'react-router-dom'
 import { OIO } from '../src'
 import OIOContainer from './pages/OIO'
 import Text from './pages/Text'
+import View from './pages/View'
 import style from './style.less'
 
 ReactDOM.render((
@@ -25,6 +26,7 @@ ReactDOM.render((
             <div className={style.nav}>
                <NavLink exact to="/" activeClassName={style.active}>OIO Container</NavLink>
                <NavLink to="/text" activeClassName={style.active}>Text</NavLink>
+               <NavLink to="/view" activeClassName={style.active}>View</NavLink>
             </div>
          </div>
          <div
@@ -37,6 +39,7 @@ ReactDOM.render((
             }}>
             <Route exact path="/" component={OIOContainer} />
             <Route path="/text" component={Text} />
+            <Route path="/view" component={View} />
          </div>
       </OIO>
    </BrowserRouter>
