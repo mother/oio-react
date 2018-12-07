5ce37ac1876306a750ed07c4d180a2f695e330c4
Ajay Sabhaney <ajay@mothercreative.com>
2018-12-07 16:25:54 -0500

    minor lint consistency thing

--- demo/pages/OIOProvider/index.js
+++ demo/pages/OIOProvider/index.js
@@ -46,7 +46,7 @@ export default class OIOProviderPage extends PureComponent {
                   <p>
                      {/* eslint-disable max-len */}
                      To use OIO components, you must wrap all components inside a <code>OIOProvider</code> component. <code>OIOProvider</code> components are used to propagate reuseable and cohesive props to all components within its scope. <code>OIOProvider</code> components can be nested.
-                     {/* eslint-enable */}
+                     {/* eslint-enable max-len */}
                   </p>
                </div>
                <div className={style.columnHalfCode}>
