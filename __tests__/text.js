f5909cb7485caddc0b257a6f3b1b60a6911c57b4
Steven Yuen <steven@mother.co>
2018-12-07 10:54:38 -0500

    Added test assertions

---
+++ __tests__/text.js
@@ -0,0 +1,15 @@
+beforeEach(() => {
+   jest.spyOn(console, 'error').mockImplementation(() => {})
+})
+
+afterEach(() => {
+   jest.restoreAllMocks()
+})
+
+test('text displays contents corrrectly', () => {
+
+})
+
+test('text has style properties', () => {
+
+})
