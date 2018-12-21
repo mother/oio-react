74a3cfd59f27101b4f03f53b4ee66301e09cb5da
Ajay Sabhaney <ajay@mothercreative.com>
2018-12-21 11:01:13 -0500

    minor updates

--- tests/withDynamicResponsiveProps.js
+++ tests/withDynamicResponsiveProps.js
@@ -8,7 +8,7 @@ import { breakpointKeys } from '../config/constants'
 
 // Test Fixture
 
-const ResponsiveText = ({ breakpoint, fontSize, fontWeight, lineHeight, value }) => (
+const UndecoratedResponsiveText = ({ breakpoint, fontSize, fontWeight, lineHeight, value }) => (
    <span style={{
       fontSize: fontSize && fontSize[breakpoint],
       fontWeight: fontWeight && fontWeight[breakpoint],
@@ -18,7 +18,7 @@ const ResponsiveText = ({ breakpoint, fontSize, fontWeight, lineHeight, value })
    </span>
 )
 
-ResponsiveText.propTypes = {
+UndecoratedResponsiveText.propTypes = {
    breakpoint: PropTypes.oneOf(breakpointKeys).isRequired,
    fontSize: OIOResponsiveObjectPropType.isRequired,
    fontWeight: OIOResponsiveObjectPropType,
@@ -26,7 +26,7 @@ ResponsiveText.propTypes = {
    value: PropTypes.string.isRequired
 }
 
-ResponsiveText.defaultProps = {
+UndecoratedResponsiveText.defaultProps = {
    fontWeight: null,
    lineHeight: null
 }
@@ -35,9 +35,9 @@ ResponsiveText.defaultProps = {
 
 test('Will work when creating dynamic props from props at each breakpoint', () => {
    // Make the line height and font weight proportional to the font size & screen size
-   const DecoratedResponsiveText = withDynamicResponsiveProps((props, breakpoint) => {
-      const fontSize = parseInt(props.fontSize[breakpoint], 10)
+   const ResponsiveText = withDynamicResponsiveProps((props, breakpoint) => {
       const { fontWeightMultiplier, lineHeightMultiplier } = props
+      const fontSize = parseInt(props.fontSize[breakpoint], 10)
       const baseCharCode = 'a'.charCodeAt(0)
       const currentCharCode = breakpoint.charCodeAt(0)
       // eslint-disable-next-line no-mixed-operators
@@ -46,9 +46,9 @@ test('Will work when creating dynamic props from props at each breakpoint', () =
          fontWeight: fontSize * fontWeightMultiplier * breakpointDiff,
          lineHeight: fontSize * lineHeightMultiplier * breakpointDiff
       })
-   })(ResponsiveText)
+   })(UndecoratedResponsiveText)
 
-   DecoratedResponsiveText.defaultProps = {
+   ResponsiveText.defaultProps = {
       fontWeightMultiplier: 1,
       fontSize: generateResponsiveObject('9[ab] 10[cd] 11[ef]'),
       lineHeightMultiplier: 0.5
@@ -56,12 +56,12 @@ test('Will work when creating dynamic props from props at each breakpoint', () =
 
    const { container } = render(
       <React.Fragment>
-         <DecoratedResponsiveText breakpoint="a" value="A [fw=9*1*1] [lh=9*0.5*1]" />
-         <DecoratedResponsiveText breakpoint="b" value="B [fw=9*1*2] [lh=9*0.5*2]" />
-         <DecoratedResponsiveText breakpoint="c" value="C [fw=10*1*3] [lh=10*0.5*3]" />
-         <DecoratedResponsiveText breakpoint="d" value="D [fw=10*1*4] [lh=10*0.5*4]" />
-         <DecoratedResponsiveText breakpoint="e" value="E [fw=11*1*5] [lh=11*0.5*5]" />
-         <DecoratedResponsiveText breakpoint="f" value="F [fw=11*1*6] [lh=11*0.5*6]" />
+         <ResponsiveText breakpoint="a" value="A [fontWeight=9*1*1] [lineHeight=9*0.5*1]" />
+         <ResponsiveText breakpoint="b" value="B [fontWeight=9*1*2] [lineHeight=9*0.5*2]" />
+         <ResponsiveText breakpoint="c" value="C [fontWeight=10*1*3] [lineHeight=10*0.5*3]" />
+         <ResponsiveText breakpoint="d" value="D [fontWeight=10*1*4] [lineHeight=10*0.5*4]" />
+         <ResponsiveText breakpoint="e" value="E [fontWeight=11*1*5] [lineHeight=11*0.5*5]" />
+         <ResponsiveText breakpoint="f" value="F [fontWeight=11*1*6] [lineHeight=11*0.5*6]" />
       </React.Fragment>
    )
 
@@ -70,23 +70,23 @@ test('Will work when creating dynamic props from props at each breakpoint', () =
 
 test('Will not fail if a dynamic prop is only defined on certain breakpoints', () => {
    // Make the line height and font weight proportional to the font size & screen size
-   const DecoratedResponsiveText = withDynamicResponsiveProps((props, breakpoint) => ({
+   const ResponsiveText = withDynamicResponsiveProps((props, breakpoint) => ({
       fontWeight: ['b', 'd', 'f'].includes(breakpoint) ? '10' : undefined,
       lineHeight: ['c', 'f'].includes(breakpoint) ? '1.1' : undefined
-   }))(ResponsiveText)
+   }))(UndecoratedResponsiveText)
 
-   DecoratedResponsiveText.defaultProps = {
+   ResponsiveText.defaultProps = {
       fontSize: generateResponsiveObject('9[ab] 10[cd] 11[ef]')
    }
 
    const { container } = render(
       <React.Fragment>
-         <DecoratedResponsiveText breakpoint="a" value="A [fw=undefined] [lh=undefined]" />
-         <DecoratedResponsiveText breakpoint="b" value="B [fw=10] [lh=undefined]" />
-         <DecoratedResponsiveText breakpoint="c" value="C [fw=undefined] [lh=1.1]" />
-         <DecoratedResponsiveText breakpoint="d" value="D [fw=10] [lh=undefined]" />
-         <DecoratedResponsiveText breakpoint="e" value="E [fw=undefined] [lh=undefined]" />
-         <DecoratedResponsiveText breakpoint="f" value="F [fw=10] [lh=1.1]" />
+         <ResponsiveText breakpoint="a" value="A [fontWeight=undefined] [lineHeight=undefined]" />
+         <ResponsiveText breakpoint="b" value="B [fontWeight=10] [lineHeight=undefined]" />
+         <ResponsiveText breakpoint="c" value="C [fontWeight=undefined] [lineHeight=1.1]" />
+         <ResponsiveText breakpoint="d" value="D [fontWeight=10] [lineHeight=undefined]" />
+         <ResponsiveText breakpoint="e" value="E [fontWeight=undefined] [lineHeight=undefined]" />
+         <ResponsiveText breakpoint="f" value="F [fontWeight=10] [lineHeight=1.1]" />
       </React.Fragment>
    )
 
