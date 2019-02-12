2952929d5757157882256bf223939f5abd99fe17
Steven Yuen <steven@mother.co>
2019-02-12 17:26:11 -0500

    Added new cypress command

--- config/cypress/support/index.js
+++ config/cypress/support/index.js
@@ -3,3 +3,11 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
 addMatchImageSnapshotCommand({
    customSnapshotsDir: '../../tests/visual/screenshots'
 })
+
+Cypress.Commands.add('testImageSnapshot', (fileName) => {
+   const env = Cypress.env()
+
+   if (env.name === 'headless') {
+      cy.matchImageSnapshot(fileName)
+   }
+})
