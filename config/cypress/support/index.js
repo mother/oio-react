27ea0e95c0bf29ddd14991bac2da8b2b470d3a33
Steven Yuen <steven@mother.co>
2019-02-22 17:48:33 -0500

    pr feedback

--- config/cypress/support/index.js
+++ config/cypress/support/index.js
@@ -6,7 +6,8 @@ addMatchImageSnapshotCommand({
 
 // Command to test Image Screenshots and match/check for visual regressions
 // Screenshot testing only takes place in headless environment where
-// we can ensure consistency
+// we can ensure consistency (we can ensure screenshots are taken at the
+// same resolution, browser window/viewport size and with same browser - electron)
 Cypress.Commands.add('testImageSnapshot', (fileName) => {
    const env = Cypress.env()
 
