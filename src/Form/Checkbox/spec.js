1cc3d391ec9f39b6c921ed71b607f6bfd1e3a962
Ajay Sabhaney <ajay@mother.co>
2019-07-17 21:22:31 -0400

    doc updates

--- src/Form/Checkbox/spec.js
+++ src/Form/Checkbox/spec.js
@@ -2,13 +2,13 @@ export default [{
    name: 'appearance',
    type: 'String (Enum)',
    default: '-',
-   description: 'This value can be one of: <code>outline</code> or <code>plain</code>. <br /><br />By default, the appearance of the component is set through the value of the nearest <code>OIOProvider</code>\'s <code>formElementAppearance</code> prop. If the <code>appearance</code> prop is set on this component, it will take precedence over the <code>appearance</code> value provided by the <code>OIOProvider</code>.',
+   description: 'This value can be one of: <code>outline</code> or <code>plain</code>. <br /><br />By default, the appearance of the component is set through the value of the nearest <code>Form</code>\'s <code>formElementAppearance</code> prop. If the <code>appearance</code> prop is set on this component, it will take precedence over the <code>appearance</code> value provided by the <code>Form</code>.',
    responsive: false
 }, {
    name: 'highlightColor',
    type: 'String',
    default: '-',
-   description: 'By default, the <code>highlightColor</code> of the component is set through the value of the nearest <code>OIOProvider</code>\'s <code>highlightColor</code> prop. If the <code>highlightColor</code> prop is set on this component, it will take precedence over the <code>highlightColor</code> value provided by the <code>OIOProvider</code>.',
+   description: 'By default, the <code>highlightColor</code> of the component is set through the value of the nearest <code>Form</code>\'s <code>highlightColor</code> prop. If the <code>highlightColor</code> prop is set on this component, it will take precedence over the <code>highlightColor</code> value provided by the <code>Form</code>.',
    responsive: false
 }, {
    name: 'id',
