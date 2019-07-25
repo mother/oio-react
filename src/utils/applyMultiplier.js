12a3c39a1f6f2462f5feacb7abd7f1614899190c
Ajay Sabhaney <ajay@mother.co>
2019-07-24 20:49:27 -0400

    Added babel optional chaining plugin & utility to apply multiplier to css like values + tests

---
+++ src/utils/applyMultiplier.js
@@ -0,0 +1,27 @@
+const cssNumValRegex = /(\D*)(-?[0-9]+(?:\.[0-9]+)?)(cm|mm|in|px|pt|pc|em|ex|ch|rem|vw|vh|vmin|vmax|%|deg|rad)?(\D*)/gi
+const unitsToIgnore = ['%', 'deg', 'rad']
+
+export default (value, scaleMultiplier, round = false) => {
+   if (typeof scaleMultiplier !== 'number') {
+      throw new Error('Scale multiplier must be a number')
+   }
+
+   if (scaleMultiplier === 1) return value
+   if (typeof value === 'number') return value * scaleMultiplier
+   if (typeof value !== 'string') return value
+
+   return value.replace(cssNumValRegex, (match, prefix, amount, unit = '', suffix) => {
+      if (unitsToIgnore.includes(unit)) return match
+
+      // Kinda hacky way to check if it was a negative number.
+      // Ideally, we would use a regex negative lookbehind assertion, but
+      // that's not supported in babel or by most browsers (yet), though it
+      // is a stage 4 tc39 proposal.
+      const isNegative = prefix.endsWith('-')
+      let number = parseFloat(amount) * scaleMultiplier
+      if (isNegative) number *= -1
+      if (round) number = Math.round(number)
+
+      return `${isNegative ? prefix.slice(0, -1) : prefix}${number}${unit}${suffix}`
+   })
+}
