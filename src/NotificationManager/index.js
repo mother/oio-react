ea9dad71af8d4035cff8dea58f8f38902c4edae2
Ajay Sabhaney <ajay@mother.co>
2019-09-12 14:53:23 -0400

    fix to prevent wonkiness with multiple NotificationManagers

--- src/NotificationManager/index.js
+++ src/NotificationManager/index.js
@@ -37,11 +37,13 @@ const NotificationManagerProvider = ({ children }) => {
       notificationContainer.current = document.createElement('div')
       notificationContainer.current.setAttribute('id', notificationContainerId)
       notificationContainer.current.style.display = 'flex'
+      notificationContainer.current.style.flexFlow = 'column'
       notificationContainer.current.style.alignItems = 'flex-end'
       notificationContainer.current.style.position = 'absolute'
       notificationContainer.current.style.bottom = 0
       notificationContainer.current.style.width = '400px'
       notificationContainer.current.style.right = 0
+      notificationContainer.current.style.padding = '24px'
       notificationContainer.current.style.pointerEvents = 'none'
       notificationContainer.current.style.fontFamily = oioContext.fontFamily
       notificationContainer.current.style.fontSize = oioContext.fontSize
@@ -70,53 +72,49 @@ const NotificationManagerProvider = ({ children }) => {
       <NotificationManagerContext.Provider value={{ showNotification }}>
          {children}
          {createPortal(
-            <View
-               float="left"
-               width="100%"
-               padding="24px">
-               <TransitionGroup>
-                  {notifications.map(notification => (
-                     <Transition
-                        key={notification.id}
-                        timeout={{
-                           appear: 0,
-                           enter: 0,
-                           exit: animationDuration
-                        }}>
-                        {(state) => {
-                           const animating = state === 'entering' || state === 'exiting'
-
-                           return (
-                              <View
-                                 width="100%"
-                                 marginTop="3px"
+            <TransitionGroup>
+               {notifications.map(notification => (
+                  <Transition
+                     key={notification.id}
+                     timeout={{
+                        appear: 0,
+                        enter: 0,
+                        exit: animationDuration
+                     }}>
+                     {(state) => {
+                        const animating = state === 'entering' || state === 'exiting'
+                        return (
+                           <View
+                              float="left"
+                              flex="0 0 auto"
+                              width="400px"
+                              marginTop="3px"
+                              borderRadius="8px"
+                              boxShadow="6px 6px 30px rgba(0,0,0,0.5)"
+                              css={{
+                                 transition: `${animationDuration}ms ease-in-out`,
+                                 opacity: animating ? '0' : '1',
+                                 transform: animating
+                                    ? 'translateX(120%)'
+                                    : 'translateX(0)'
+                              }}>
+                              <NotificationInline
+                                 backgroundColor="rgba(40,40,40,0.95)"
                                  borderRadius="8px"
-                                 boxShadow="6px 6px 30px rgba(0,0,0,0.5)"
-                                 css={{
-                                    transition: `${animationDuration}ms ease-in-out`,
-                                    opacity: animating ? '0' : '1',
-                                    transform: animating
-                                       ? 'translateX(120%)'
-                                       : 'translateX(0)'
-                                 }}>
-                                 <NotificationInline
-                                    backgroundColor="rgba(40,40,40,0.95)"
-                                    borderRadius="8px"
-                                    message={notification.message}
-                                    iconSpacing="24px"
-                                    paddingHorizontal="24px"
-                                    paddingVertical="24px"
-                                    textColor="#fff"
-                                    title={notification.title}
-                                    type={notification.type}
-                                 />
-                              </View>
-                           )
-                        }}
-                     </Transition>
-                  ))}
-               </TransitionGroup>
-            </View>,
+                                 message={notification.message}
+                                 iconSpacing="24px"
+                                 paddingHorizontal="24px"
+                                 paddingVertical="24px"
+                                 textColor="#fff"
+                                 title={notification.title}
+                                 type={notification.type}
+                              />
+                           </View>
+                        )
+                     }}
+                  </Transition>
+               ))}
+            </TransitionGroup>,
             notificationContainer.current
          )}
       </NotificationManagerContext.Provider>
