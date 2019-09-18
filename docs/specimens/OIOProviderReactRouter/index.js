ea825f1e4c04f433535bc10f65d53d356c06152d
Ajay Sabhaney <ajay@mother.co>
2019-09-18 16:43:47 -0400

    doc fix

--- docs/specimens/OIOProviderReactRouter/index.js
+++ docs/specimens/OIOProviderReactRouter/index.js
@@ -5,7 +5,7 @@ OIOProviderReactRouterSpecimen.title = 'React Router Integration'
 OIOProviderReactRouterSpecimen.description = 'Some of the button components that also act as links can be flexibly configured to work with a custom router. For example, if we wanted to use the <code>Link</code> component from React Router (instead of a plain old <code>a</code> tag), we could configure this to be used by OIO by passing in the <code>buttonLinkAdapter</code> option.'
 
 OIOProviderReactRouterSpecimen.codeSnippet = `
-const buttonLinkAdapter = () => {
+const buttonLinkAdapter = {
    render: ({ linkTo, linkReplace, ...props }) => (
       <Link to={linkTo} replace={linkReplace} {...props} />
    )
