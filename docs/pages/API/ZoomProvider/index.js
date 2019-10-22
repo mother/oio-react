cc9d26f899fee981c9b7b36aee5b21f228590af5
Steven Yuen <stevenyuen@Stevens-MacBook-Pro.local>
2019-10-22 00:39:43 -0400

    Added docs and specimens

--- docs/pages/API/ZoomProvider/index.js
+++ docs/pages/API/ZoomProvider/index.js
@@ -3,12 +3,14 @@ import APITableBlock from '../../../components/APITableBlock'
 import HeaderBlock from '../../../components/HeaderBlock'
 import SpecimenBlock from '../../../components/SpecimenBlock'
 import zoomProviderSpec from '../../../../src/ZoomProvider/spec'
+import ZoomContextSpecimen from '../../../specimens/ZoomContext'
 import ZoomProviderSpecimen from '../../../specimens/ZoomProvider'
 
 const ZoomProviderPage = () => (
    <div>
       <HeaderBlock title="ZoomProvider" />
       <SpecimenBlock specimen={ZoomProviderSpecimen} />
+      <SpecimenBlock specimen={ZoomContextSpecimen} />
       <APITableBlock spec={zoomProviderSpec} title="ZoomProvider API Options" />
    </div>
 )
